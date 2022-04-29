9417711049f14bc140349081a57d39c7e635edf9
olag-grayhair <coldworld77777@gmail.com>
2022-04-29 18:20:15 +0300

    day 3, finaly?

--- cpu.py
+++ cpu.py
@@ -1,23 +1,21 @@
 import psutil
 import PySimpleGUI as sg
-import time
 import os
 
 kernel = os.uname()[2]
 version = os.uname()[3].split(' ')[0].split('~')[1]
 machine = os.uname()[4]
+cpu_count = psutil.cpu_count()
 
 def uptime():
     return os.popen('uptime -p').read()[:-1].split('up')[1].replace(' ', '', 1)
+
 def cpu_mhz():
-    # print(psutil.cpu_count())
-    # print(current_cpu)
     real = round(psutil.cpu_freq().current, 3)
     current_cpu = str(real).replace('.', '')
     if len(current_cpu) < 4:
         current_cpu = current_cpu + '0'
     current_cpu = int(current_cpu)
-    print(current_cpu)
     return current_cpu
 
 def color_use():
@@ -25,7 +23,7 @@ def color_use():
     if round(percent) > 70:
         return '#9B2335'
     elif round(percent) > 50:
-        return '#955251'
+        return '#FFD700'
     elif round(percent) > 30:
         return '#FF6F61'
     else:
@@ -52,35 +50,23 @@ def mem_use():
     percent = psutil.virtual_memory().percent
     # print(f'available: {round(available, 2)}G use: {round(used, 2)}G percent: {percent}% total: {round(total, 1)}G')
     return total, percent, used, available
-
 # print(psutil.cpu_stats())
 
 def temperatures():
     temp = psutil.sensors_temperatures()
     cpu_temp = rep(str(temp['coretemp'][0]), 3)
     gpu_temp = rep(str(temp['radeon'][0]), 1)
-    # print(temp)
-    # cpu_temp1 = rep(str(temp['coretemp'][1]), 2)
-    # cpu_temp2 = rep(str(temp['coretemp'][2]), 2)
-    # cpu_temp3 = rep(str(temp['coretemp'][3]), 2)
-    # cpu_temp4 = rep(str(temp['coretemp'][4]), 2)
     return gpu_temp, cpu_temp
 #gui
 sg.theme('black')
-
-# sg.theme_previewer()
 cpu = [
     [sg.Text('CPU usage'), sg.Text('1999MHz', pad=(80, 0), key='mhz', text_color='#00FF00')],
-          [sg.ProgressBar(100, orientation='h', size=(20, 20), key='cpu1'), sg.Text('0%', key='cpu%1', text_color='#00FF00')],
-          [sg.ProgressBar(100, orientation='h', size=(20, 20), key='cpu2'), sg.Text('0%', key='cpu%2', text_color='#00FF00')],
-          [sg.ProgressBar(100, orientation='h', size=(20, 20), key='cpu3'), sg.Text('0%', key='cpu%3', text_color='#00FF00')],
-          [sg.ProgressBar(100, orientation='h', size=(20, 20), key='cpu4'), sg.Text('0%', key='cpu%4', text_color='#00FF00')],
           [sg.Text('CPU temperature'), sg.Text('0', key='cpu_temp', text_color='#00FF00', pad=(50, 0))],
           [sg.Text('GPU temperature'), sg.Text('0', key='gpu_temp', text_color='#00FF00', pad=(50, 0))],
        ]
 
 mem = [
-         [sg.Text('Memory', pad=(0, 0)), sg.Text('1999MHz', pad=(80, 0), key='mhz', text_color='#00FF00')],
+         [sg.Text('Memory', pad=(0, 0))],
          [sg.ProgressBar(100, orientation='h', pad=(0, 0), bar_color=(color_use(), '#FFF'), size=(20, 20), key='mem'),
           sg.Text('0G', key='memText', pad=(10, 0), text_color='#00FF00')]
 ]
@@ -90,19 +76,28 @@ date = [
     [sg.Text('Distribution: '), sg.Text(f'{version} {machine}', text_color='#00FF00', pad=(40, 0))],
     [sg.Text('Kernel: '), sg.Text(kernel, text_color='#00FF00', pad=(79, 0))],
 ]
+
+bars = [
+[sg.ProgressBar(100, orientation='h', size=(20, 20), key=f'cpu{i+1}'),
+ sg.Text('0%', key=f'cpu%{i+1}', text_color='#00FF00')] for i in range(0, cpu_count)]
+
 layout = [
     [sg.Column(date)],
     [sg.HorizontalSeparator(pad=(0, 20))],
     [sg.Column(cpu)],
+    bars,
     [sg.HorizontalSeparator(pad=(0, 20))],
     [sg.Column(mem)],
-    [sg.Button('Quit', pad=(0, 10))]
+    [sg.Button('Quit', pad=(5, 30))]
 ]
 
 # 'winnative', 'clam', 'alt', 'classic', 'vista', 'xpnative'
-window = sg.Window('Custom Progress Meter', layout, no_titlebar=True,
-                   location=(700, 30), margins=(10, 20),
-
+window = sg.Window('Custom Progress Meter', layout,
+                   no_titlebar=True,
+                   location=(870, 30),
+                    alpha_channel=0.7,
+                   margins=(10, 0),
+                    size=(410,500),
                    font=('Ubuntu', 11, 'bold'))
 # loop that would normally do something useful
 while True:
@@ -111,20 +106,18 @@ while True:
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
 
-    # cpu1, cpu2, cpu3, cpu4 = usage()
     cpu = usage()
-    window['cpu1'].UpdateBar(cpu[0])
-    window['cpu2'].UpdateBar(cpu[1])
-    window['cpu3'].UpdateBar(cpu[2])
-    window['cpu4'].UpdateBar(cpu[3])
+    for i in range(0, cpu_count):
+        window[f'cpu{i + 1}'].UpdateBar(cpu[i])
+
     #cpu_temp
     gpu_temp, cpu_temp = temperatures()
     window['cpu_temp'].update(f'{cpu_temp}℃')
     window['gpu_temp'].update(f'{gpu_temp}℃')
-    window['cpu%1'].update(f'{round(cpu[0])}%')
-    window['cpu%2'].update(f'{round(cpu[1])}%')
-    window['cpu%3'].update(f'{round(cpu[2])}%')
-    window['cpu%4'].update(f'{round(cpu[3])}%')
+
+    for i in range(0, cpu_count):
+        window[f'cpu%{i+1}'].update(f'{round(cpu[i])}%')
+
     window['mhz'].update(f'{cpu_mhz()}MHz')
     #mem
     total, percent, used, available = mem_use()
@@ -134,3 +127,4 @@ while True:
     window['uptime'].update(f'{uptime()}')
 
 window.close()
+
