4d7ee51228e53fc7066df94fa8d1cd9acaa34518
olag-grayhair <coldworld77777@gmail.com>
2022-04-30 09:46:56 +0300

    weather on half end

--- pygui.py
+++ pygui.py
@@ -1,17 +1,28 @@
 import PySimpleGUI as sg
 import requests
+import datetime
 
-import schedule
-import time
 # Define the window's contents
 API = 'bf51f443afaae5d5c8d1cec8ba83608c'
 
-def cities(name):
-    if geo(name).status_code == 200 and name != '':
-        print('good city')
+def clocker():
+    now = datetime.datetime.now()
+    if now.hour > 22 and now.hour < 5:
+        print('n')
+        return 'Night'
+    elif now.hour > 5 and now.hour < 12:
+        return 'Morning'
+    elif now.hour > 12 and now.hour < 18:
+        return 'Afternoon'
     else:
-        print('bad city')
-        return
+        return 'Evening'
+
+# def cities(name):
+#     if geo(name).status_code == 200 and name != '':
+#         print('good city')
+#     else:
+#         print('bad city')
+#         return
 
 def geo(city_feeld):
     return requests.get(f'https://api.openweathermap.org/data/2.5/weather?q='
@@ -36,16 +47,16 @@ def weather_data(name):
     #GUI INTERFACE
 description, temp, img_data, city, name = weather_data('kharkov')
 
+sg.theme('DarkBlue1')
 layout = [
           [sg.Input(key='-INPUT-', size=30), sg.Button('Ok')],
           [sg.Text(city, font=('Philosopher, 26'), key='-CITY-')],
           [sg.Text(f"{temp} ℃", font=('Philosopher, 26'), key='-TEMP-'), sg.Image(img_data, key='-IMG-')],
-          [sg.Text(description, key='-DESCRIPTION-')],
-          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Text(description, key='-DESCRIPTION-'), sg.Text(f'{clocker()}', pad=(30, 10), key='clock')],
           # [sg.Button('Ok'), sg.Button('Quit')]
 ]
 # Create the window
-window = sg.Window('Weather', layout)
+window = sg.Window('Weather', layout, alpha_channel=0.8)
 # Display and interact with the Window using an Event Loop
 while True:
     event, values = window.read(timeout=30000)
